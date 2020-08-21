1ef53572378e5b7a9709ec8e679b2ec9a544b8cd
22mackenzie <22mackenzie.butler@boiseschools.net>
2020-08-21 15:04:19 +0000

    ASCII art

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/*Mackenzie Butler
+AP computer science
+8/20/2020
+*/
+ 
+public class HelloWorld{
+ public static void main(String args[]){
+   System.out.println("");
+   System.out.println("");
+   System.out.println("() ()");
+   System.out.println("('.')");
+   System.out.println("(UU)o");
+   System.out.println("");
+   System.out.println("");
+   System.out.println("|-----------------|");
+   System.out.println("|     coding      |");
+   System.out.println("|     is Fun      |");
+   System.out.println("|-----------------|");
+ 
+ }
+}
